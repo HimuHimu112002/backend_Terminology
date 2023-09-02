3c9ba64cb4ee1c59a51f87a9fc9570d7de723c34
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-09-02 11:51:12 +0600

    terminology

---
+++ index.js
@@ -0,0 +1,12 @@
+// ======================= Good json format =========
+// 1 -> 
+
+// }
+//     "data":[
+//         "name": "himu",
+//     ]
+    // jokhon json array niye kaj korbo sei time json array akti name deoya best
+// {
+
+// 2 -> json key and value single cottition er majhe na rekhe ke value double cottition rakah best
+// 3 -> Name convension firstName best then first_name good then first-name very bad
