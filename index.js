387ce4ea9047eed19cf5ebbed75c795989e1e914
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-09-03 11:40:41 +0600

    terminology

--- index.js
+++ index.js
@@ -10,3 +10,28 @@
 
 // 2 -> json key and value single cottition er majhe na rekhe ke value double cottition rakah best
 // 3 -> Name convension firstName best then first_name good then first-name very bad
+
+// =============== Status code meaning ======================
+// 1 -> 200 -> = OK
+// 2 -> 201 -> = CREATED
+// 3 -> 202 -> = ACCEPTED
+// 4 -> 203 -> = NO AUTHORATATIVE
+// 5 -> 204 -> = NO CONTENT
+// 6 -> 205 -> = RESET CONTENT
+// 7 -> 206 -> = PARTIAL CONTENT
+// 8 -> 400 -> = BAD REQUEST
+// 9 -> 401 -> = UNAUTHORIZED
+// 9 -> 403 -> = FORBIDEN
+// 10 -> 404 -> = ERROR
+// 10 -> 405 -> = MATHOD NOT ALLOWED
+// 11 -> 500 -> = INTERNAL SERVER ERROR
+
+
+
+// ========================= URL NAMING SYSTEM ================
+
+// 1 -> url name amon vabe sajate hobe jeno url dekhe kisu idea pauya na jai asole ay url kiser jonno
+// and jodi url dekhe bujhte pare keu tahole security issue hoiye jabe
+
+// =================== COOKIE ===========================
+// MOST IMPORTANT VALO KORE SIKHTE HOBE
